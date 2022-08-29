4a7a9c52004232c4ead7160341141b18343e956d
GokaChandrasrinivas <97425173+GokaChandrasrinivas@users.noreply.github.com>
2022-08-29 10:55:50 +0530

    n=int(input())#28 sum=0 for i in range(1,n):#28 4     if(n%i==0):         sum=sum+i#1+2+4+7+14 if(sum==n):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,9 @@
-n=int(input())
-s=0
-for i in  range(1,n//2+1):
-    if n%i==0:
-        s=s+i
-if s==n:
-    print(True)
+n=int(input())#28
+sum=0
+for i in range(1,n):#28 4
+    if(n%i==0):
+        sum=sum+i#1+2+4+7+14
+if(sum==n):
+    print("True")
 else:
-    print(False)
+    print("False")
