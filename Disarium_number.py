4737c84b98c5c19dae3db84fe4128bfe366e26a3
GokaChandrasrinivas <97425173+GokaChandrasrinivas@users.noreply.github.com>
2022-09-12 20:00:57 +0530

    n=int(input()) temp=n temp1=n c=0
    while(n>0):
        r=n%10
        c=c+1
        n=n//10
    m=c
    r=0
    s=0
    for i in range(m,0,-1):
        r=temp%10
        s=s+r**i
        temp=temp//10
    if(s==temp1):
        print(True)
    else:
        print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,18 +1,21 @@
 n=int(input())
-c=0
-s=0
 temp=n
-while n>0:
+temp1=n
+c=0
+ 
+while(n>0):
     r=n%10
-    n//=10
-    c+=1
-n=temp
-while n>0:
-    r=n%10
-    s=s+pow(r,c)
+    c=c+1
     n=n//10
-    c-=1
-if s==temp:
-    print("True")
+m=c
+r=0
+s=0
+for i in range(m,0,-1):
+    r=temp%10
+    s=s+r**i
+    temp=temp//10
+if(s==temp1):
+    print(True)
 else:
-    print("False")
+    print(False)
+     
