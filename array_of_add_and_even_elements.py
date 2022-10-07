ca8b4927c6882b6bc5352663cef1d3cb7605ee02
GokaChandrasrinivas <97425173+GokaChandrasrinivas@users.noreply.github.com>
2022-10-07 17:27:34 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] for i in l:     if(i%2==1):         l1.append(i) for i in l:     if(i%2==0):         l1.append(i) for i in l1:     print(i,end=" ")

---
+++ array_of_add_and_even_elements.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in l:
+    if(i%2==1):
+        l1.append(i)
+for i in l:
+    if(i%2==0):
+        l1.append(i)
+for i in l1:
+    print(i,end=" ")
+    
