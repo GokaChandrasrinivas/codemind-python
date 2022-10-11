b4da5fcc5bc20a8d1b7adadd26e4a06c5c8ba699
GokaChandrasrinivas <97425173+GokaChandrasrinivas@users.noreply.github.com>
2022-10-11 14:55:46 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] for i in l:     if(i in l1):         continue     else:         l1.append(i) l2=[] c=0 for i in l1:     if(l.count(i)==i):         c=c+1         l2.append(i) if(c==0):     print(-1) else:     print(min(l2),max(l2))

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,16 +1,18 @@
 n=int(input())
 l=list(map(int,input().split()))
 l1=[]
-l2=[]
 for i in l:
     if(i in l1):
         continue
     else:
         l1.append(i)
+l2=[]
+c=0
 for i in l1:
     if(l.count(i)==i):
+        c=c+1
         l2.append(i)
-if(len(l2)==0):
-    print("-1")
+if(c==0):
+    print(-1)
 else:
     print(min(l2),max(l2))
